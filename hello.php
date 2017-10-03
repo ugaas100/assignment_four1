71ce478f24bac5c7303dcb2e45855bc9b273f2a1
Abdisamad Ugas <qy7670el@metrostate.edu>
2017-10-03 17:57:40 -0500

    First commit

---
+++ hello.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bob's Auto Parts - Order Form</title>
+</head>
+<body>
+<?php
+echo "Hello world!<br />\n";
+echo "Good by world";
+?>
+</body>
+</html>
